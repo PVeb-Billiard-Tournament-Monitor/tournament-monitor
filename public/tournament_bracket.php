ff314105e2517955477d71ae50173f71a4e99941
Djordje Milicevic <milicevicdjordje94@gmail.com>
2016-12-29 02:30:40 +0100

    Indent fix

--- public/tournament_bracket.php
+++ public/tournament_bracket.php
@@ -10,34 +10,34 @@
 	<script type="text/javascript">
 
 		var tournamentData = {
-		  	"teams":
-		  	[
-		    	["Ivan Ivanovic", "Marko Markovic"],
-		    	["Jovan Jovanovic", "Alekandar Aleksandrovic"],
-		    	["Nikola Nikolic", "Milos Milosevic"],
-		    	["Stefan Stefanovic", "Dejan Dejanovic"]
-		  	],
-		  	"results":
-		  	[
-		    	[
-		    		// first match
-		      		[
-		        		[22, 11],
-		        		[10, 4],
-		        		[41, 5],
-		        		[52, 6]
-		        	],
-		        	// second match
-		        	[
-		        		[11, 24],
-		        		[42, 55]
-		        	],
-		        	// third match
-		        	[
-		        		[22, 53],
-		        	]
-		    	]
-		  	]
+			"teams":
+			[
+				["Ivan Ivanovic", "Marko Markovic"],
+				["Jovan Jovanovic", "Alekandar Aleksandrovic"],
+				["Nikola Nikolic", "Milos Milosevic"],
+				["Stefan Stefanovic", "Dejan Dejanovic"]
+			],
+			"results":
+			[
+				[
+					// first match
+					[
+						[22, 11],
+						[10, 4],
+						[41, 5],
+						[52, 6]
+					],
+					// second match
+					[
+						[11, 24],
+						[42, 55]
+					],
+					// third match
+					[
+						[22, 53],
+					]
+				]
+			]
 		}
 
 		$('.tree').bracket({
